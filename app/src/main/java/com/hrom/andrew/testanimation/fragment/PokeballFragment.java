8c0659e9b103a320c7b253e6324217c5ade1b7d9
Andrew Grom <groshkka@gmail.com>
2016-02-29 19:30:38 +0200

    create formula for coordinate y

--- app/src/main/java/com/hrom/andrew/testanimation/fragment/PokeballFragment.java
+++ app/src/main/java/com/hrom/andrew/testanimation/fragment/PokeballFragment.java
@@ -23,6 +23,10 @@ import com.hrom.andrew.testanimation.R;
 import java.util.Random;
 
 public class PokeballFragment extends Fragment {
+    private float x = 0f;
+    private float y = 0f;
+    private float radius = 155f;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.pokeball, container, false);
@@ -35,11 +39,8 @@ public class PokeballFragment extends Fragment {
         final ImageView pokeball_2 = (ImageView) getActivity().findViewById(R.id.pokeball2);
         ImageView pokeball_3 = (ImageView) getActivity().findViewById(R.id.pokeball3);
 
-        Animation animSnowFlake = AnimationUtils.loadAnimation(getContext(), R.anim.main_ball);
-
-        Random r = new Random();
+        final Random r = new Random();
         int random = r.nextInt(30);
-
 
         /*if (0 <= random & random < 10) {
             pokeball_1.startAnimation(animSnowFlake);
@@ -54,21 +55,39 @@ public class PokeballFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Log.d("coordinate", "pokeball_1");
-                ObjectAnimator animator4 = ObjectAnimator.ofFloat(pokeball_2, "translationX", 0F, -210F);
+                ObjectAnimator animator4 = ObjectAnimator.ofFloat(pokeball_2, "translationX", 0F, -310F);
                 AnimatorSet set1 = new AnimatorSet();
                 set1.play(animator4);
                 set1.setDuration(3000);
                 set1.start();
                 Log.d("coordinate", String.valueOf(animator4.getCurrentPlayTime()));
 
-                ObjectAnimator animator1 = ObjectAnimator.ofFloat(pokeball_1, "translationX", 0F, 210F);
-                ObjectAnimator animator2 = ObjectAnimator.ofFloat(pokeball_1, "translationY", -105F, 0F);
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(pokeball_1, "translationX", 0F, 310F);
+                animator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        Log.d("coordinate", "x = " + String.valueOf(animation.getAnimatedValue()));
+                        x = Float.parseFloat(animation.getAnimatedValue().toString());
+                    }
+                });
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(pokeball_1, "translationY", 0F, 0F);
+                animator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        Log.d("coordinate", "y = " + String.valueOf(animation.getAnimatedValue()));
+                        y = (float) Math.sqrt(radius * radius - x * x);
+                        animation.setFloatValues(y);
+                    }
+                });
                 AnimatorSet set = new AnimatorSet();
                 set.play(animator1).with(animator2);
                 set.setDuration(3000);
                 set.start();
                 Log.d("coordinate", String.valueOf(animator2.getAnimatedValue("translationY")));
                 Log.d("coordinate", String.valueOf(animator1.getAnimatedValue("translationX")));
+
+                ValueAnimator valueAnimator = ValueAnimator.ofFloat();
+                //valueAnimator.addUpdateListener();
             }
         });
 
